728bd19684b8b44e99d7ea9f25708b2b3d3b8b5a
Patrick Scully <patrick@possibleweb.com>
2018-10-02 08:36:57 -0400

    Initial Set up of Weather App

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Titles extends Component {
+    render() { 
+        return ( 
+            <div>
+                <h1>Golf Weather Finder</h1>
+                <p>Find out temperature, conditions, and more for your round today.</p>
+            </div>
+         );
+    }
+}
+ 
+export default Titles;
---
+++ src/config.js
@@ -0,0 +1,3 @@
+const API_KEY = 'c71339e64c5916c350bbccce2a194af5';
+
+export { API_KEY };
